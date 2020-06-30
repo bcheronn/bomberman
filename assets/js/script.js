f8cd36ae79241f7411285157a8b786dfe21d0366
Bertrand C <66913901+bcheronn@users.noreply.github.com>
2020-06-30 10:56:54 +0200

    Keys parsed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,19 @@
-window.addEventListener("keydown", () => console.log("window keydown"));
+function keydownLst(event) {
+  const keyPressed = event.code;
+  switch (keyPressed) {
+    case "ArrowUp":
+      return console.log("Up : " + keyPressed);
+    case "ArrowDown":
+      return console.log("Down : " + keyPressed);
+    case "ArrowLeft":
+      return console.log("Left : " + keyPressed);
+    case "ArrowRight":
+      return console.log("Right : " + keyPressed);
+    case "Space":
+      return console.log("Space : " + keyPressed);
+    default:
+      return console.log("Others : " + keyPressed);
+  }
+}
+
+window.addEventListener("keydown", (evt) => keydownLst(evt));
